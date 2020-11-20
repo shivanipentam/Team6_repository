21221ecd1b8a5a9c9e4e71a614bdf418c751198b
shivani <shivani@DESKTOP-51942DG>
2020-11-20 11:29:35 +0530

    controller

--- PLP1/src/main/java/com/cg/controller/App.java
+++ PLP1/src/main/java/com/cg/controller/App.java
@@ -1,5 +1,15 @@
 package com.cg.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class App {
+	static Logger myLogger=LogManager.getLogger(App.class);
+
+    public static void doWork() {
+
+        myLogger.info("App doWork() called");
+    }
 
 }
--- PLP1/src/main/java/com/cg/controller/UserController.java
+++ PLP1/src/main/java/com/cg/controller/UserController.java
@@ -1,5 +1,25 @@
 package com.cg.controller;
 
-public class UserController {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID=1L;
+	static Logger log=LogManager.getLogger(UserController.class);
+	public UserController() {
+		super();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		log.info("Added");
+	
+	
+	}
 }
